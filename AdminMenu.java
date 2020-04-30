383c2110884495541c86140f19d2460c6b739706
pragowda <pragowda@iu.edu>
2020-04-30 18:30:22 -0500

    Implemented add and remove flight methods for AdminMenu and added getters and setters for Flight class

--- AdminMenu.java
+++ AdminMenu.java
@@ -1,40 +1,40 @@
 
 public class AdminMenu extends Flight {
 	
-	protected String type;
-	protected String date;
-	protected String source;
-	protected String dest;
-	protected String airline;
-	protected int adultTic;
-	protected int childTic;
+    protected String type;
+    protected String date;
+    protected String source;
+    protected String dest;
+    protected String airline;
+    protected int adultTic;
+    protected int childTic;
 	
-	public AdminMenu(String type, String date, String source, String dest, String airline, int adultTic, int childTic) {
-		super(type, date, source, dest, airline, adultTic, childTic);
+    public AdminMenu(String type, String date, String source, String dest, String airline, int adultTic, int childTic) {
+	super(type, date, source, dest, airline, adultTic, childTic);
 		
-	}
+    }
 	
-	public void addFlight() {
-		
-	}
+    public void addFlight(Flight addF) { // Adds flight addF to flights
+	addFlightSuper(addF); // Uses addFlightSuper method in Flight superclass
+    }
 	
-	public void removeFlight(Flight removeF) {
-		//removes flight from flights
-	}
+    public void removeFlight(Flight removeF) { // Removes flight removeF from flights
+	removeFlightSuper(removeF); // Uses removeFlightSuper method in Flight superclass
+    }
 	
-	public void addPoints(Account acc) {
-		//adds points to user's account if flight is cancelled
-	}
+    public void addPoints(Account acc) {
+	//adds points to user's account if flight is cancelled
+    }
 	
-	public void blacklist(Account user) {
-		//restricts a user from using logged in airline
-	}
+    public void blacklist(Account user) {
+	//restricts a user from using logged in airline
+    }
 	
-	public void giftPoints(Account user, int points) {
-		//adds inputted points to inputted user's account
-	}
+    public void giftPoints(Account user, int points) {
+	//adds inputted points to inputted user's account
+    }
 	
-	 public void reviewView() {
+    public void reviewView() {
 		 
-	 }
+    }
 }
--- Flight.java
+++ Flight.java
@@ -2,26 +2,36 @@ import java.util.ArrayList;
 
 public class Flight {
 
-	protected ArrayList<Flight> flights = new ArrayList<>();
-	protected String type;
-	protected String date;
-	protected String source;
-	protected String dest;
-	protected String airline;
-	protected int adultTic;
-	protected int childTic;
+    protected ArrayList<Flight> flights = new ArrayList<>();
+    protected String type;
+    protected String date;
+    protected String source;
+    protected String dest;
+    protected String airline;
+    protected int adultTic;
+    protected int childTic;
 	
-	public Flight(String type, String date, String source, String dest, String airline, int adultTic, int childTic) {
-		super();
-		this.type = type;
-		this.date = date;
-		this.source = source;
-		this.dest = dest;
-		this.airline = airline;
-		this.adultTic = adultTic;
-		this.childTic = childTic;
+    public Flight(String type, String date, String source, String dest, String airline, int adultTic, int childTic) {
+	super();
+	this.type = type;
+	this.date = date;
+	this.source = source;
+	this.dest = dest;
+	this.airline = airline;
+	this.adultTic = adultTic;
+	this.childTic = childTic;
 		
-	}
-	
-	
+    }
+
+    public ArrayList<Flight> getFlights() { // Returns flights
+	return flights;
+    }
+
+    public void addFlightSuper(Flight f) { // Adds inputted flight to flights
+	flights.add(f);
+    }
+
+    public void removeFlightSuper(Flight f) { // Removes inputted flight from flights
+	flights.remove(f);
+    }
 }
