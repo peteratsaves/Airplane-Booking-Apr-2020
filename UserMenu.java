e7f762c9fcb80649eff80995a7b833e39602a92d
pragowda <pragowda@iu.edu>
2020-05-01 13:39:29 -0500

    Removed random f character and some minor bracketting errors. Did not address any other issues

--- UserMenu.java
+++ UserMenu.java
@@ -2,60 +2,62 @@
 public class UserMenu {
 	
 
-	protected String type;
-	protected String date;
-	protected String source;
-	protected String dest;
-	protected String airline;
-	protected int adultTic;
-	protected int childTic;
+    protected String type;
+    protected String date;
+    protected String source;
+    protected String dest;
+    protected String airline;
+    protected int adultTic;
+    protected int childTic;
 	
 	
 	
-	public static void userMenu() {
-		System.out.println("Please enter the following information to find a flight.");
+    public static void userMenu() {
+	System.out.println("Please enter the following information to find a flight.");
 		
-	}
+    }
 	
-	public Flight generateFlight() {
-		Scanner obj = new Scanner(System.in);
-		String info = obj.nextLine();
-		for(int i=0; i<flights.length; i++){
-			if (flights.gets(i)==info){
-				return flights.get(i);
-			}
-		}
-f		//takes input from UserMenu and generates a new flight to be compared to existing flights
-		return null;
+    public Flight generateFlight() {
+	Scanner obj = new Scanner(System.in);
+	String info = obj.nextLine();
+	for(int i=0; i<flights.length; i++) {
+	    if (flights.gets(i)==info) {
+		return flights.get(i);
+	    }
 	}
+	//takes input from UserMenu and generates a new flight to be compared to existing flights
+	return null;
+    }
 	
-	public void search(Flight userFlight) {
-		//compares generated flight to existing flight
+    public void search(Flight userFlight) {
+	//compares generated flight to existing flight
 		
-	}
+    }
 	
-	public void book() {
-		Flight x = generateFlight();
-		flights.add(x);
-		//adds chosen flight to users Account
-	}
+    public void book() {
+	Flight x = generateFlight();
+	flights.add(x);
+	//adds chosen flight to users Account
+    }
 	
-	public void filter(Flight userFlight) {
-		//displays flights matching selected parameters of generated flight
-	}
+    public void filter(Flight userFlight) {
+	//displays flights matching selected parameters of generated flight
+    }
 	
-	public void sorting() {
-		for(int i=0; i<flights.length; i++){
-			int x = i;
-			while (flights.gets(x+1).date<flights.get(x).date{
-				Flight temp = flights.get(x);
-				flights(x) = flights.get(x+1);
-				flights(x+1) = temp;
-				x--;
+    public void sorting() {
+	for(int i=0; i<flights.length; i++) {
+	    int x = i;
+	    while (flights.gets(x+1).date<flights.get(x).date) {
+		Flight temp = flights.get(x);
+		flights(x) = flights.get(x+1);
+		flights(x+1) = temp;
+		x--;
 		//displays existing flights in ascending or descending order based off of user input
+	    }
 	}
-	
-	public void reviewCreate() {
+    }
+
+    public void reviewCreate() {
 		
-	}
+    }
 }
